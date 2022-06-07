8b609effb2a1c5cfa6022cd17374460ef335fd1e
Micaela Analia Jofre <mkjofre@gmail.com>
2022-06-07 17:23:03 -0300

    email

---
+++ contact.php
@@ -0,0 +1,13 @@
+<?php
+    $nombre = $_POST['name'];
+    $email = $_POST['email'];
+    $tel = $_POST['phone'];
+    $mensaje = $_POST['detail'];
+    $formcontent="Nombre: $nombre \n E-mail: $email \n Telefono: $tel \n Mensaje: $mensaje";
+    $recipient = "consultas@micaelajofre.ml";
+    $subject = "Consultas";
+    $header = "From: $email \r\n";
+    $header .= "Content-Type: text/plain; charset=UTF-8";
+    mail($recipient, $subject, $formcontent, $header) or die("Error!");
+    header("Location:  ../index.html");
+?>
